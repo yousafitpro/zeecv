d668e1df5a09c0db4b9100a1dad8d6c0a905d585
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-08-29 00:59:34 +0500

    Update bottom_tabs.dart

--- lib/widgets/bottom_tabs.dart
+++ lib/widgets/bottom_tabs.dart
@@ -6,51 +6,163 @@ import '../core/constants/app_colors.dart';
 class BottomTabs extends StatelessWidget {
   final int selectedIndex;
   const BottomTabs({
-      Key? key,
-      required this.selectedIndex,
-    }) : super(key: key);
+    Key? key,
+    required this.selectedIndex,
+  }) : super(key: key);
+
+  void _onTap(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/home/find-jobs');
+        break;
+      case 1:
+        context.go('/home/my-jobs');
+        break;
+      case 2:
+        context.go('/in-app/edit-resume');
+        break;
+      case 3:
+        context.go('/home/receivers'); // placeholder, replace with your route
+        break;
+      case 4:
+        context.go('/home/profile');
+        break;
+    }
+  }
 
   @override
-  Widget build(BuildContext context){
-    return  BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              currentIndex: this.selectedIndex,
-              onTap: (index) {
-                switch (index) {
-                  case 0:
-                    context.go('/home/find-jobs');
-                    break;
-                  case 1:
-                    context.go('/home/my-jobs');
-                    break;
-                  case 2:
-                    context.go('/in-app/edit-resume');
-                    break;
-                  case 3:
-                    context.go('/home/profile');
-                    break;
-                }
-              },
-              selectedItemColor: AppColors.primary,
-              unselectedItemColor: Colors.grey,
-              items: const [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.search),
-                  label: 'Find Job',
+  Widget build(BuildContext context) {
+    final bottomInset = MediaQuery.of(context).padding.bottom;
+
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        top: false,
+        child: SizedBox(
+          height: 98, // bar content height; SafeArea adds the inset below
+          child: Stack(
+            clipBehavior: Clip.none,
+            alignment: Alignment.topCenter,
+            children: [
+              // Background bar
+              Positioned.fill(
+                top: 20,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.06),
+                        blurRadius: 10,
+                        offset: const Offset(0, -2),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      _NavItem(
+                        icon: Icons.home_filled,
+                        label: 'Home',
+                        isSelected: selectedIndex == 0,
+                        onTap: () => _onTap(context, 0),
+                      ),
+                      _NavItem(
+                        icon: Icons.swap_horiz,
+                        label: 'Find Jobs',
+                        isSelected: selectedIndex == 1,
+                        onTap: () => _onTap(context, 1),
+                      ),
+                      const SizedBox(width: 64), // space for center button
+                      _NavItem(
+                        icon: Icons.swap_horiz,
+                        label: 'My Jobs',
+                        isSelected: selectedIndex == 3,
+                        onTap: () => _onTap(context, 3),
+                      ),
+                      _NavItem(
+                        icon: Icons.people_alt_outlined,
+                        label: 'Profile',
+                        isSelected: selectedIndex == 4,
+                        onTap: () => _onTap(context, 4),
+                      )
+                    ],
+                  ),
                 ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.work),
-                  label: 'My Jobs',
+              ),
+              // Floating center button
+              Positioned(
+                top: 0,
+                child: GestureDetector(
+                  onTap: () => _onTap(context, 2),
+                  child: Container(
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: AppColors.primary,
+                      border: Border.all(color: Colors.white, width: 4),
+                      boxShadow: [
+                        BoxShadow(
+                          color: AppColors.primary.withOpacity(0.35),
+                          blurRadius: 12,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: const Icon(
+                      Icons.description,
+                      color: Colors.white,
+                      size: 26,
+                    ),
+                  ),
                 ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.description),
-                  label: 'Edit Resume',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.person),
-                  label: 'Profile',
-                ),
-              ],
-            );
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _NavItem extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const _NavItem({
+    required this.icon,
+    required this.label,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final color = isSelected ? AppColors.primary : Colors.grey;
+    return InkWell(
+      onTap: onTap,
+      customBorder: const CircleBorder(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(icon, color: color, size: 24),
+            const SizedBox(height: 4),
+            Text(
+              label,
+              style: TextStyle(
+                color: color,
+                fontSize: 11,
+                fontWeight: isSelected ? FontWeight.w600 : FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
