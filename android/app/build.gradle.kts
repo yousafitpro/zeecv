7c9917afaf63dbd102981a8c4b47cdbb2f811bb7
Muhammad Yousaf <77118786+yousafitpro@users.noreply.github.com>
2026-08-30 15:07:55 +0500

    zz

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.zeecv"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 200
-        versionName = "2.0.0"
+        versionCode = 201
+        versionName = "2.0.1"
     }
 
     signingConfigs {
